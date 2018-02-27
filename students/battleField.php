7b9b2156621c2eb182195b19d33ea6cfcb176931
Eric LE DEVEDEC <ledevedec.eric@gmail.com>
2018-02-27 11:08:05 +0100

    Projet fini

--- students/battleField.php
+++ students/battleField.php
@@ -6,6 +6,31 @@
 
   class BattleField extends BaseBattleField
   {
+        public static function createMyWarrior () {
+
+            $guerrier = new StartrekWarrior($GLOBALS['warriorID']);
+
+            $weapon = new Weapon('lame', 100);
+            $weapon->SetImageUrl('http://www.courantpositif.fr/wp-content/uploads/2013/06/Morpho.jpg');
+
+            $guerrier->SetWeapon($weapon);
+            $guerrier->SetImageUrl('https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/68dd54ca-60cf-4ef7-898b-26d7cbe48ec7/10-dithering-opt.jpg');
+            $guerrier->saveNew();
+        }
+
+
+        public static function createOtherWarrior () {
+            $guerrier = new StartrekWarrior('other');
+
+            $weapon = new Weapon('lame', 100);
+            $weapon->SetImageUrl('http://i63.servimg.com/u/f63/12/53/27/92/tekoka10.jpg');
+
+            $guerrier->SetWeapon($weapon);
+            $guerrier->SetImageUrl('https://demo.phpgang.com/crop-images/demo_files/pool.jpg');
+            $guerrier->saveNew();
+        }
+
+
 
 
   }
--- students/warrior.php
+++ students/warrior.php
@@ -3,9 +3,121 @@
 require_once __DIR__ . "/../base/localWarrior.php";
 
 
-$GLOBALS['warriorID'] = 'azertyuiop';
+$GLOBALS['warriorID'] = 'monIdRiko';
 
 // Définissez vos class de combattants
-abstract class Warrior extends LocalWarrior {
+abstract class Warrior extends LocalWarrior
+{
 
-};
+    public $id, $name, $speed, $life, $shield, $imageUrl, $weapon;
+
+    public function __construct($id, $speed = 30, $life = 100, $shield = 20)
+    {
+
+        $this->id = $id;
+        $this->speed = $speed;
+        $this->life = $life;
+        $this->shield = $shield;
+    }
+
+
+    public function SetWeapon(Weapon $weapon)
+    {
+
+        return $this->weapon = $weapon;
+
+    }
+
+    public function SetImageUrl($url)
+    {
+
+        $this->imageUrl = $url;
+    }
+
+}
+
+;
+
+
+class StartrekWarrior extends Warrior
+{
+    public $mentalPower;
+
+
+    public function __construct($id, $speed = 30, $life = 100, $shield = 20, $mentalPower = 8)
+    {
+        $this->mentalPower = $mentalPower;
+
+        parent::__construct($id, $speed, $life, $shield);
+
+    }
+
+
+    public function power()
+    {
+
+        return $this->mentalPower;
+    }
+
+
+}
+
+class MarvelWarrior extends Warrior
+{
+    public $superPower;
+
+    public function __construct($id, $speed = 30, $life = 100, $shield = 20, $superPower = 100)
+    {
+        $this->superPower = $superPower;
+
+        parent::__construct($id, $speed, $life, $shield);
+
+    }
+
+    public function power()
+    {
+
+        return $this->superPower;
+    }
+
+}
+
+class PokemonWarrior extends Warrior
+{
+    public $level;
+
+    public function __construct($id, $speed = 30, $life = 100, $shield = 20, $level = 1)
+    {
+        $this->level = $level;
+
+        parent::__construct($id, $speed, $life, $shield);
+
+    }
+
+
+    public function power()
+    {
+
+        return $this->level;
+    }
+
+}
+
+
+class Weapon
+{
+
+    public $id, $strength, $imageUrl;
+
+    public function __construct($id, $strength)
+    {
+        $this->id = $id;
+        $this->strength = $strength;
+    }
+
+    public function SetImageUrl($url)
+    {
+        $this->imageUrl = $url;
+
+    }
+}
